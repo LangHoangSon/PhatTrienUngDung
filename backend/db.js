2f6e666b41707edde011770af136a4143e4e8db3
HoangSon <langhoangson25012005@gmail.com>
2026-04-11 10:14:35 +0700

    Hoan thien

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '', // XAMPP mặc định là rỗng
+  password: '123456', // XAMPP mặc định là rỗng
   database: 'restaurant_qr'
 });
 
--- backend/server.js
+++ backend/server.js
@@ -97,6 +97,7 @@ app.get('/test-db', (req, res) => {
 // });
 app.get('/api/generate-qrs', async (req, res) => {
 
+  const frontendHost = process.env.FRONTEND_HOST || 'localhost';
   const [tables] = await db.promise().query(
     "SELECT table_number FROM tables"
   );
@@ -109,7 +110,7 @@ app.get('/api/generate-qrs', async (req, res) => {
 
   for (const table of tables) {
 
-    const url = `http://10.34.37.6:3000/menu?table=${table.table_number}`;
+    const url = `http://${frontendHost}:3000/menu?table=${table.table_number}`;
 
     const filePath = path.join(folder, `table-${table.table_number}.png`);
 
